d4bc6459cfe109f432885171ca19124855a260bf
Daniel Vassalo <daniel.vassalo@hotmail.com.br>
2022-08-17 09:35:58 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ cd dist
 git init
 git checkout -b main
 git add -A
+git config --global user.name "dzou"
+git config --global user.email "dzou@company.com"
 git commit -m 'deploy'
 git push -f git@github.com:vassalo/lec-config-builder.git main:gh-pages
 cd -
