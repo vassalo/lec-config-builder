d242bc9887a76666d71af781ebe91f58ecc0fe8f
vassalo <daniel.vassalo@hotmail.com.br>
2022-08-18 15:08:33 -0300

    Fix release workflow
    Co-authored-by: wallrony <wallrony@users.noreply.github.com>

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,7 +1,7 @@
 git pull --recurse-submodules
 git submodule init
 git submodule update --remote --recursive
-git config --global user.name 'Git bot'
-git config --global user.email 'bot@noreply.github.com'
+git config --global user.name 'vassalo'
+git config --global user.email $GH_USER_EMAIL
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
 git commit -am "Auto updated submodule references" && git push || echo "No changes to commit"
